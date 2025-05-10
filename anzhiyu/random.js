52e9f71b57bae7e4a007566a693c85abb31c4bd5
hsqdd24 <ylei.6351@gmail.com>
2025-05-10 22:06:43 +0800

    Site updated: 2025-05-10 22:06:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/09/这是一篇新的博文/","2025/03/13/hello-world/"];function toRandomPost(){
+var posts=["article/这是一篇新的博文/","article/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
